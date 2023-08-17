36d3c76ea5a529aba8b1698f465e890a4d293d1c
Kamalahasan A <dev.iamkamal@gmail.com>
2023-08-17 09:44:04 +0530

    Next Js Template with Src Directory

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <>
+      <h1 className={`font-bold text-3xl`}>Hello World</h1>
+    </>
+  )
+}
